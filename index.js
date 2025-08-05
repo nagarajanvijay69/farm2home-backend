0176c2accd8a7120846c0c0393a9b27f69ec2483
nagarajanvijay69 <nagarajanvijay69@gmail.com>
2025-08-05 21:19:28 +0530

    save

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookie());
-app.use(cors());
+app.use(cors({
+   credentials : true
+}));
 
 mongoose.connect('mongodb+srv://nagarajanvijay69:nagarajanvijay...@cluster0.vcydudb.mongodb.net/data')
    .then((res) => {

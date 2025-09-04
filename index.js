58026cbe6ffbcaba98ed5292590d1d36c739c55c
nagarajanvijay69 <nagarajanvijay69@gmail.com>
2025-09-04 20:52:43 +0530

    fix: update CORS origin to an array format

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookie());
 
 app.use(cors({
-   origin: 'https://farm2home-e2kl.onrender.com',
+   origin: ['https://farm2home-e2kl.onrender.com'],
    credentials: true,
    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
    allowedHeaders: ["Content-Type", "Authorization"],

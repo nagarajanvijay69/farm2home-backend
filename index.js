6c0ea6eda69aea280166219f69e33a79fe1209db
nagarajanvijay69 <nagarajanvijay69@gmail.com>
2026-08-06 08:12:07 +0530

    added PATCH in cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cookie());
 app.use(cors({
    origin: ['https://farm2home-frontend-puce.vercel.app', 'http://localhost:5173'],
    credentials: true,
-   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
    allowedHeaders: ["Content-Type", "Authorization"],
    exposedHeaders: ["set-cookie"],
 }));
